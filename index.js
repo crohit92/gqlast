5b840997dfb382d067b024d028d7595a19cffdf3
Rohit Chopra <rohit.chopra@kochartech.com>
2019-05-10 19:10:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+/**
+ * Convert a template string to graphql abstract syntax tree
+ */
+var valueSeperatorInObjectKeys = ',';
+function gqlast(literals, ...placeholders) {
+    debugger
+    const result = [...literals];
+    for (let index = 0; index < placeholders.length; index++) {
+        parsedPlaceholder = parsePlaceholder(placeholders[index]);
+        result[index] += parsedPlaceholder;
+    }
+    return result.join(' ');
+}
+
+function parsePlaceholder(placeholder) {
+    return (typeof placeholder === typeof '') ?
+        `"${placeholder}"` :
+        (typeof placeholder === typeof 0 || typeof placeholder === typeof true ? placeholder :
+            (placeholder instanceof Array ? parseArray(placeholder) :
+                (placeholder instanceof Date ? `"${placeholder.toISOString()}"` :
+                    parseObject(placeholder)
+                )
+            )
+        );
+
+}
+
+function parseArray(array) {
+    let result = '[';
+    // for (const value, index} of array) {
+    array.forEach((value, index) => {
+        result += `${parsePlaceholder(value)} ${index < array.length - 1 ? valueSeperatorInObjectKeys : ''}`;
+    });
+    result += ']';
+    return result;
+}
+
+function parseObject(object) {
+    let result = '{';
+    const keys = Object.keys(object)
+    keys.forEach((key, index) => {
+        result += `${key}: ${parsePlaceholder(object[key])} ${index < keys.length - 1 ? valueSeperatorInObjectKeys : ''}`;
+    })
+
+    result += '}';
+    return result;
+}
+module.exports = gqlast;
